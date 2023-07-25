bd699927377d191ed6ec0f91ad830f575647467c
programmer <info2016frei@gmail.com>
2023-07-25 16:18:49 +0200

    fixed the descirbe plot

--- 1_Project_Predict_Customer_Churn/utils.py
+++ 1_Project_Predict_Customer_Churn/utils.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import logging
@@ -55,3 +56,72 @@ def save_plot(df: pd.DataFrame, feature: str, save_path: str) -> None:
     # Save the plot to a file
     plt.savefig(save_path)
     logging.info(f"Saved plot to {save_path}")
+
+
+
+def save_describtion(df: pd.DataFrame, save_path: str, file_name: str) -> None:
+    """ Save descriptive statistics to image file
+    Args:
+        df (pd.DataFrame): Pandas DataFrame
+        save_path (str): Path to save plot
+        file_name (str): Name of file
+    """
+    # some assertions
+    assert isinstance(df, pd.DataFrame), "df should be a Pandas DataFrame"
+    assert isinstance(save_path, str), "save_path should be a string"
+    assert isinstance(file_name, str), "file_name should be a string"
+
+    # drop column
+    # import pdb; pdb.set_trace()
+    drop_features = [f'Naive_Bayes_Classifier_Attrition_Flag_Card_Category_Contacts_Count_12_mon_Dependent_count_Education_Level_Months_Inactive_12_mon_{i}' for i in range(1, 3)]
+    for feature in drop_features:
+        if feature in df.columns:
+            df = df.drop(columns=[feature])
+            logging.info(f"Dropped {feature} from df")
+        else:
+            logging.info(f"{feature} not in df")
+
+    
+    save_path = os.path.join(save_path, 'descriptive_statistics.png')
+    # if already image already exists return
+    if os.path.exists(save_path):
+        logging.info(f"Image already exists at {save_path}")
+        return
+    # Get descriptive statistics
+    desc_stats = df.describe().round(2)
+
+   
+    # import pdb; pdb.set_trace()
+    # Split the columns into two sets
+    # TODO fix this plots are false
+    # Save the tables as images
+    desc_stats = df.describe().round(2)
+     
+    fig, ax = plt.subplots()
+    # Adjust bbox values as needed to center the table
+    # Create the table and set cell padding
+    table = plt.table(cellText=desc_stats.values.T,
+                    colLabels=desc_stats.index,
+                    rowLabels=desc_stats.columns,
+                    cellLoc='center',
+                    loc='center',
+                    )
+
+    table.auto_set_column_width(col=list(range(len(desc_stats.columns))))
+    ax.axis('off')
+    plt.savefig(save_path, bbox_inches='tight')
+    logging.info(f"Saved plot to {save_path}")
+
+
+
+
+
+
+
+
+
+   
+    
+
+
+
